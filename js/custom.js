bd2ea12fbfe07c64f31d715c39e81e3a96d06e32
NICOLAS YIP Z. W. <nicolas.yip@tricorsenedi.com>
2014-11-25 01:04:59 +0800

    Improvise and tidy up some javascript.
    There was some messy leftover code. But the trigger button still does
    not change the content.
    
    Have to find another way.

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,3 @@
-$(window).load(function () {
+$(window).load(function() {
 	$("#content").load("ajax-pages/welcome.html");
-	$("#welcomeNo").on("click", function() {
-		$("#content").load("ajax-pages/thank-you.html");
-	});
 });
